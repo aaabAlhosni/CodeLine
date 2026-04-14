7a70479a7255ea36de161bb0441e7bfc1c514f43
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-14 15:10:17 +0400

    start: Declare # as a varraible.

---
+++ CodeLine/src/tasks/month1/sprint1/TheStaircaseChallenge.java
@@ -0,0 +1,12 @@
+package tasks.month1.sprint1;
+
+public class TheStaircaseChallenge {
+    public static void main(String[] args) {
+        //Declare
+        String Hash = "#";
+
+
+        //print
+    }
+
+}

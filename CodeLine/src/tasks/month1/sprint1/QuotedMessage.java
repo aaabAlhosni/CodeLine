90d89a45582ba69080e3b388e73d174079bada2f
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:52:42 +0400

    QuotedMessage added some explaining comments

--- CodeLine/src/tasks/month1/sprint1/QuotedMessage.java
+++ CodeLine/src/tasks/month1/sprint1/QuotedMessage.java
@@ -1,9 +1,12 @@
 package tasks.month1.sprint1;
 
+/**
+ * QuotedMessage - Prints a phrase with embedded quotation marks.
+ * Uses escape character \" to display quotes in the output without ending the string.
+ */
 public class QuotedMessage {
     public static void main(String[] args) {
+        // Prints the phrase with inner quotes using escape sequence \"
         System.out.println("He said, \"Practice makes perfect!\"");
-    }
-};
     }
 }

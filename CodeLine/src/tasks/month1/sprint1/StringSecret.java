11e0fdcf0d949a44c983208f9d939825878cffb8
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:24:37 +0400

    finished StringSecret

--- CodeLine/src/tasks/month1/sprint1/StringSecret.java
+++ CodeLine/src/tasks/month1/sprint1/StringSecret.java
@@ -3,6 +3,6 @@ package tasks.month1.sprint1;
 public class StringSecret {
     public static void main(String[] args) {
         String text = "Java Programming";
-        System.out.println(text.length());
+        System.out.println("Length: " + text.length());
     }
 }

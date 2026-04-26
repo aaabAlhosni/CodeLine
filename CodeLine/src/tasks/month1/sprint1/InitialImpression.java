24ff4981b806951ea88e4444cfad037c7b4e45cc
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:55:02 +0400

    InitialImpression added some explaining comments

--- CodeLine/src/tasks/month1/sprint1/InitialImpression.java
+++ CodeLine/src/tasks/month1/sprint1/InitialImpression.java
@@ -1,8 +1,15 @@
 package tasks.month1.sprint1;
 
+/**
+ * InitialImpression - Demonstrates extracting the first character from a string.
+ * Uses charAt() method to get the initial (first letter) from a full name.
+ */
 public class InitialImpression {
     public static void main(String[] args) {
+        // Full name variable containing first and last name
         String fullName = "John Doe";
+        // Extract first character (index 0) using charAt(0) method
+        // This outputs 'J' (the initial)
         System.out.println(fullName.charAt(0));
     }
 }

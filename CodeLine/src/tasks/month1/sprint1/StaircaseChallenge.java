e521292796ec559c5c2bd2dd7cd1efc00f9a0d73
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:16:54 +0400

    add third step

--- CodeLine/src/tasks/month1/sprint1/StaircaseChallenge.java
+++ CodeLine/src/tasks/month1/sprint1/StaircaseChallenge.java
@@ -4,5 +4,6 @@ public class StaircaseChallenge {
     public static void main(String[] args) {
         System.out.println("#");
         System.out.println("##");
+        System.out.println("###");
     }
 }

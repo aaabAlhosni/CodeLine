a54396bebd3df1508b5b64d4f7e30fc7e2e8e87f
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:23:57 +0400

    add John record

--- CodeLine/src/tasks/month1/sprint1/StudentInfo.java
+++ CodeLine/src/tasks/month1/sprint1/StudentInfo.java
@@ -4,6 +4,7 @@ public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Name\tAge\tGrade");
         System.out.println("John\t20\tA");
+        System.out.println("John\t20\tA");
         System.out.println("Sarah\t22\tB");
     }
 }

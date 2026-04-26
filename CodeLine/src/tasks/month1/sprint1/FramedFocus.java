49dfda5911312fc35daefca2d611ec5a49ef11c2
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:18:02 +0400

    add message line

--- CodeLine/src/tasks/month1/sprint1/FramedFocus.java
+++ CodeLine/src/tasks/month1/sprint1/FramedFocus.java
@@ -4,5 +4,6 @@ public class FramedFocus {
     public static void main(String[] args) {
         System.out.println("***********************");
         System.out.println("* *");
+        System.out.println("* \"Keep Learning\"   *");
     }
 }

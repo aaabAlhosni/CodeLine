fdbb874ad0db548c62bb4df4d2e03e5f2a5420bf
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:24:53 +0400

    finished CharacterCraft

--- CodeLine/src/tasks/month1/sprint1/CharacterCraft.java
+++ CodeLine/src/tasks/month1/sprint1/CharacterCraft.java
@@ -3,5 +3,6 @@ package tasks.month1.sprint1;
 public class CharacterCraft {
     public static void main(String[] args) {
         String word = "Learning";
+        System.out.println(word.charAt(4));
     }
 }

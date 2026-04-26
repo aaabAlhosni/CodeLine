8a19b2e99210e673f07ab7664dc5b65637c27db3
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:21:34 +0400

    add title

--- CodeLine/src/tasks/month1/sprint1/MultiplicationHeader.java
+++ CodeLine/src/tasks/month1/sprint1/MultiplicationHeader.java
@@ -3,5 +3,6 @@ package tasks.month1.sprint1;
 public class MultiplicationHeader {
     public static void main(String[] args) {
         System.out.println("=========================================");
+        System.out.println("           MULTIPLICATION TABLE");
     }
 }

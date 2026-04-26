bfa205c416d6ee0f80605d871888e10e2e845d56
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:21:26 +0400

    start MultiplicationHeader

---
+++ CodeLine/src/tasks/month1/sprint1/MultiplicationHeader.java
@@ -0,0 +1,7 @@
+package tasks.month1.sprint1;
+
+public class MultiplicationHeader {
+    public static void main(String[] args) {
+        System.out.println("=========================================");
+    }
+}

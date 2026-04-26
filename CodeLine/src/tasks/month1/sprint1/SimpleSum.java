3c52090f535e9bd64d31fc0730d303eee4467604
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:53:45 +0400

    SimpleSum added some explaining comments

--- CodeLine/src/tasks/month1/sprint1/SimpleSum.java
+++ CodeLine/src/tasks/month1/sprint1/SimpleSum.java
@@ -1,10 +1,18 @@
 package tasks.month1.sprint1;
 
+/**
+ * SimpleSum - Demonstrates basic integer addition in Java.
+ * Adds two integer variables and prints the result.
+ */
 public class SimpleSum {
     public static void main(String[] args) {
+        // First integer operand
         int num1 = 15;
+        // Second integer operand
         int num2 = 25;
+        // Calculate sum using basic addition operator
         int sum = num1 + num2;
+        // Print the result (outputs: 40)
         System.out.println(sum);
     }
 }

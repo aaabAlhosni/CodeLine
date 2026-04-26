3e7ad83c956f2075558bbee31c824dd33b3f8254
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:27:25 +0400

    finished InitialImpression

--- CodeLine/src/tasks/month1/sprint1/InitialImpression.java
+++ CodeLine/src/tasks/month1/sprint1/InitialImpression.java
@@ -3,5 +3,6 @@ package tasks.month1.sprint1;
 public class InitialImpression {
     public static void main(String[] args) {
         String fullName = "John Doe";
+        System.out.println(fullName.charAt(0));
     }
 }

dd297798ebcc564d3de802959509ca0a43e89264
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:51:01 +0400

    LongSeparator added some explaining comments

--- CodeLine/src/tasks/month1/sprint1/LongSeparator.java
+++ CodeLine/src/tasks/month1/sprint1/LongSeparator.java
@@ -1,9 +1,12 @@
 package tasks.month1.sprint1;
 
+/**
+ * LongSeparator - Prints exactly 50 equal signs in a single line.
+ * Used as a visual separator in formatted output.
+ */
 public class LongSeparator {
     public static void main(String[] args) {
+        // Prints 50 equal signs (=) as a decorative line separator
         System.out.println("==================================================");
-    }
-};
     }
 }

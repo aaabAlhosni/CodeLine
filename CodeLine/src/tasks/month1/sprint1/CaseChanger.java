54d2a0d07fb70388b8aaffe1609e51c5714e3a54
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:25:08 +0400

    add uppercase

--- CodeLine/src/tasks/month1/sprint1/CaseChanger.java
+++ CodeLine/src/tasks/month1/sprint1/CaseChanger.java
@@ -3,5 +3,6 @@ package tasks.month1.sprint1;
 public class CaseChanger {
     public static void main(String[] args) {
         String text = "Sprint One";
+        System.out.println(text.toUpperCase());
     }
 }

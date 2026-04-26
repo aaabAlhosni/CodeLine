f52d17edc7ee0827e63a0e2dfa39632ddd0fb590
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:21:42 +0400

    add bottom border

--- CodeLine/src/tasks/month1/sprint1/MultiplicationHeader.java
+++ CodeLine/src/tasks/month1/sprint1/MultiplicationHeader.java
@@ -4,5 +4,6 @@ public class MultiplicationHeader {
     public static void main(String[] args) {
         System.out.println("=========================================");
         System.out.println("           MULTIPLICATION TABLE");
+        System.out.println("=========================================");
     }
 }

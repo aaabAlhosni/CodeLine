103c2c75314d77b00bc43a1ded6c580be06cbd01
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:24:15 +0400

    finished StudentInfo

--- CodeLine/src/tasks/month1/sprint1/StudentInfo.java
+++ CodeLine/src/tasks/month1/sprint1/StudentInfo.java
@@ -4,7 +4,8 @@ public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Name\tAge\tGrade");
         System.out.println("John\t20\tA");
-        System.out.println("John\t20\tA");
         System.out.println("Sarah\t22\tB");
+    }
+}
     }
 }

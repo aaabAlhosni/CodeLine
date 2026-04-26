3910655293ecc63de7118b3d49b1654828801e19
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:18:53 +0400

    finished StarMatrix

--- CodeLine/src/tasks/month1/sprint1/StarMatrix.java
+++ CodeLine/src/tasks/month1/sprint1/StarMatrix.java
@@ -6,5 +6,6 @@ public class StarMatrix {
         System.out.println("* * * * *");
         System.out.println("* * * * *");
         System.out.println("* * * * *");
+        System.out.println("* * * * *");
     }
 }

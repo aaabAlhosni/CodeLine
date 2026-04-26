6caa0683316dcbdeb19262e4d829c98cd5ef5f4a
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:23:48 +0400

    add header row

--- CodeLine/src/tasks/month1/sprint1/StudentInfo.java
+++ CodeLine/src/tasks/month1/sprint1/StudentInfo.java
@@ -2,6 +2,8 @@ package tasks.month1.sprint1;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println("Name\tAge\tGrade\nJohn\t20\tA\nSarah\t22\tB");
+        System.out.println("Name\tAge\tGrade");
+        System.out.println("John\t20\tA");
+        System.out.println("Sarah\t22\tB");
     }
 }

654056842ef948f1ec6e787d97725405e3b92aa1
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:52:21 +0400

    StudentInfo added some explaining comments

--- CodeLine/src/tasks/month1/sprint1/StudentInfo.java
+++ CodeLine/src/tasks/month1/sprint1/StudentInfo.java
@@ -1,11 +1,16 @@
 package tasks.month1.sprint1;
 
+/**
+ * StudentInfo - Displays a simple table of student information.
+ * Uses tab (\t) and newline (\n) escape sequences for formatted output.
+ */
 public class StudentInfo {
     public static void main(String[] args) {
+        // Table header with column names separated by tabs
         System.out.println("Name\tAge\tGrade");
+        // First student record: Name=John, Age=20, Grade=A
         System.out.println("John\t20\tA");
+        // Second student record: Name=Sarah, Age=22, Grade=B
         System.out.println("Sarah\t22\tB");
-    }
-}
     }
 }

a10d8967eca67393e189da5c1f991b8afb3465d9
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:22:51 +0400

    add second line

--- CodeLine/src/tasks/month1/sprint1/NumberLadder.java
+++ CodeLine/src/tasks/month1/sprint1/NumberLadder.java
@@ -3,5 +3,6 @@ package tasks.month1.sprint1;
 public class NumberLadder {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("1\t2");
     }
 }

bfbda80f9f2b7d063d40fe3e605553c90f99fdf4
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:18:38 +0400

    add third row

--- CodeLine/src/tasks/month1/sprint1/StarMatrix.java
+++ CodeLine/src/tasks/month1/sprint1/StarMatrix.java
@@ -4,5 +4,6 @@ public class StarMatrix {
     public static void main(String[] args) {
         System.out.println("* * * * *");
         System.out.println("* * * * *");
+        System.out.println("* * * * *");
     }
 }

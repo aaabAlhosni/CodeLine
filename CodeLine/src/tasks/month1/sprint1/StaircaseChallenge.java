04b088c6dd6c750e8c247a881a4acf9b17b986be
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:50:18 +0400

    StaircaseChallenge added some explaining comments

--- CodeLine/src/tasks/month1/sprint1/StaircaseChallenge.java
+++ CodeLine/src/tasks/month1/sprint1/StaircaseChallenge.java
@@ -1,11 +1,20 @@
 package tasks.month1.sprint1;
 
+/**
+ * StaircaseChallenge - Prints a staircase pattern using hash symbols.
+ * Demonstrates basic output printing without loops or conditionals.
+ */
 public class StaircaseChallenge {
     public static void main(String[] args) {
+        // First step: single hash
         System.out.println("#");
+        // Second step: two hashes
         System.out.println("##");
+        // Third step: three hashes
         System.out.println("###");
+        // Fourth step: four hashes
         System.out.println("####");
+        // Fifth step: five hashes (top of staircase)
         System.out.println("#####");
     }
 }

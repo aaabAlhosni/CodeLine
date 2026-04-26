f2e3fc8e3ce4b9d6cc23825f0a26fbc001e57a6f
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-26 17:19:32 +0400

    finished AsteriskFrame

--- CodeLine/src/tasks/month1/sprint1/AsteriskFrame.java
+++ CodeLine/src/tasks/month1/sprint1/AsteriskFrame.java
@@ -4,5 +4,7 @@ public class AsteriskFrame {
     public static void main(String[] args) {
         System.out.println("*****");
         System.out.println("* *");
+        System.out.println("* *");
+        System.out.println("*****");
     }
 }

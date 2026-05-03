0c106c718e7fe05316e14fca31beda11c78ddc4b
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-03 18:39:32 +0400

    add second middle line to AsteriskFrame

--- CodeLine/main/src/java/tasks/month1/sprint1/AsteriskFrame.java
+++ CodeLine/main/src/java/tasks/month1/sprint1/AsteriskFrame.java
@@ -4,5 +4,6 @@ public class AsteriskFrame {
     public static void main(String[] args) {
         System.out.println("*****");
         System.out.println("*   *");
+        System.out.println("*   *");
     }
 }

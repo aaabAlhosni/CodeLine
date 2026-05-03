f64919f280c0702c744ae450407ef3a80a691af4
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-03 18:51:39 +0400

    add print before swap to VariableSwap

--- CodeLine/main/src/java/tasks/month1/sprint1/VariableSwap.java
+++ CodeLine/main/src/java/tasks/month1/sprint1/VariableSwap.java
@@ -4,5 +4,6 @@ public class VariableSwap {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
+        System.out.println("Before swap: a = " + a + ", b = " + b);
     }
 }

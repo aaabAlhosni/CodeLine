1485bed80b08c985d3c57d9e287e15ef6d0cb445
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-03 18:50:04 +0400

    finished NameWeaver

--- CodeLine/main/src/java/tasks/month1/sprint1/NameWeaver.java
+++ CodeLine/main/src/java/tasks/month1/sprint1/NameWeaver.java
@@ -4,5 +4,7 @@ public class NameWeaver {
     public static void main(String[] args) {
         String firstName = "John";
         String lastName = "Doe";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
     }
 }

952d7c604d0289c787d0b4d04c4761d610971703
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-03 18:52:00 +0400

    finished VariableSwap

--- CodeLine/main/src/java/tasks/month1/sprint1/VariableSwap.java
+++ CodeLine/main/src/java/tasks/month1/sprint1/VariableSwap.java
@@ -5,5 +5,9 @@ public class VariableSwap {
         int a = 10;
         int b = 20;
         System.out.println("Before swap: a = " + a + ", b = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swap: a = " + a + ", b = " + b);
     }
 }

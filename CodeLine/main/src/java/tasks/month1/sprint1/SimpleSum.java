dae1cebc7cd880f7635fff7d6f0b76a8565f97f6
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-03 18:45:01 +0400

    finished SimpleSum

--- CodeLine/main/src/java/tasks/month1/sprint1/SimpleSum.java
+++ CodeLine/main/src/java/tasks/month1/sprint1/SimpleSum.java
@@ -4,5 +4,7 @@ public class SimpleSum {
     public static void main(String[] args) {
         int num1 = 15;
         int num2 = 25;
+        int sum = num1 + num2;
+        System.out.println(sum);
     }
 }

6ff1d226e0601fb7a11bf4751e10e4ca20687f7a
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-03 18:40:18 +0400

    start MultiplicationHeader

---
+++ CodeLine/main/src/java/tasks/month1/sprint1/MultiplicationHeader.java
@@ -0,0 +1,11 @@
+package tasks.month1.sprint1;
+
+public class MultiplicationHeader {
+    public static void main(String[] args) {
+        System.out.println("=========================================");
+        System.out.println("           MULTIPLICATION TABLE");
+        System.out.println("=========================================");
+        System.out.println("1\t2\t3\t4\t5\t6\t7\t8\t9\t10");
+        System.out.println("-----------------------------------------");
+    }
+}

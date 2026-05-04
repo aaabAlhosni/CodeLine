42e114841b0b7bb71895434235016ff67b4e5fb4
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-04 12:30:05 +0400

    Commit 11: Add characters 3710-4081 of 22228

--- CodeLine/main/src/java/tasks/month1/sprint2/CliErp.java
+++ CodeLine/main/src/java/tasks/month1/sprint2/CliErp.java
@@ -78,4 +78,10 @@ public class CilErp {
                                     //Displaying complains
                                     int currentComplain = complains.indexOf(complainIndex);
                                     System.out.printf("Complain ID: #[%s]\n", complains.get(currentComplain));
-                                   
+                                    System.out.printf("Description: %s\n", descriptions.get(currentComplain));
+
+                                    //changeing proiorty
+                                    System.out.println("proiorty : Set to [Medium]Default , do you wat to change it (Y/n)?");
+                                    String changeProiorty = scannerBuffer.nextLine();
+
+                        

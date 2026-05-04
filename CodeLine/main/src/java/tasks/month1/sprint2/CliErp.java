7434e24d592def0d24cf4bcf958f8828976ebf3d
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-04 12:30:03 +0400

    Commit 1: Add characters 0-371 of 22228

---
+++ CodeLine/main/src/java/tasks/month1/sprint2/CliErp.java
@@ -0,0 +1,14 @@
+package tasks.month1.sprint2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CilErp {
+    public static void main(String[] args) {
+//  Implemeting a scanner
+        Scanner scannerBuffer = new Scanner(System.in);
+
+//Input Storing
+        ArrayList<Integer> complains = new ArrayList<Integer>();
+        ArrayList<String> de

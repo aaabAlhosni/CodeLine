bb5a525f4939ef1028f8fd043418f821f93ebaa3
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-05 10:16:38 +0400

    Add challenge title print statement

--- CodeLine/main/src/java/tasks/month2/sprint3/TheStaircaseChallenge.java
+++ CodeLine/main/src/java/tasks/month2/sprint3/TheStaircaseChallenge.java
@@ -4,3 +4,5 @@ public class TheStaircaseChallenge {
     public static void main(String[] args) {
 
         int rowsExample = 5;
+        //============================================================
+        System.out.println("Step Up: The Staircase Challenge");

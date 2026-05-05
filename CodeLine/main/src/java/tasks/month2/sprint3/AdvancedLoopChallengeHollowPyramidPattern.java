b0da78f5e22994a22a171d8877c4c5e48dbe7cc4
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-05 10:54:13 +0400

    Implement main method and initialize numOfRows variable

--- CodeLine/main/src/java/tasks/month2/sprint3/AdvancedLoopChallengeHollowPyramidPattern.java
+++ CodeLine/main/src/java/tasks/month2/sprint3/AdvancedLoopChallengeHollowPyramidPattern.java
@@ -1,3 +1,8 @@
 package tasks.month2.sprint3;
 
 public class AdvancedLoopChallengeHollowPyramidPattern {
+    public static void main(String[] args) {
+
+        int numOfRows = 6;
+//===============================================================
+    //  outer loop

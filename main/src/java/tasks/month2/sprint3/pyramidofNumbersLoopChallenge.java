03ec375dca83b7346e7ea8a757bb4e3a2bb7293f
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-05 11:19:58 +0400

    Add print statements and comment separator

--- main/src/java/tasks/month2/sprint3/pyramidofNumbersLoopChallenge.java
+++ main/src/java/tasks/month2/sprint3/pyramidofNumbersLoopChallenge.java
@@ -4,3 +4,6 @@ public class pyramidofNumbersLoopChallenge {
     public static void main(String[] args) {
 
         int numOfrows = 4;
+//===============================================================
+        System.out.println();
+        System.out.println("Pyramid of Numbers: Loop Challenge");

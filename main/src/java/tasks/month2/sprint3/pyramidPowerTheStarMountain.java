0787bc1e8835827c6a733fcaf2ac42d80f1b5ae5
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-05 11:26:52 +0400

    Add println, close loops, and add ending comment

--- main/src/java/tasks/month2/sprint3/pyramidPowerTheStarMountain.java
+++ main/src/java/tasks/month2/sprint3/pyramidPowerTheStarMountain.java
@@ -13,3 +13,8 @@ public class pyramidPowerTheStarMountain {
 
                 System.out.print("*");
             }
+            System.out.println();
+        }
+//==================================================================
+    }
+}

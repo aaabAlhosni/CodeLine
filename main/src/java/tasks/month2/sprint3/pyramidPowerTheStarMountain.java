0db2458d1792c9b31dc51f2b98d7c57fe45d0aec
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-05 11:25:29 +0400

    Add comment separator

--- main/src/java/tasks/month2/sprint3/pyramidPowerTheStarMountain.java
+++ main/src/java/tasks/month2/sprint3/pyramidPowerTheStarMountain.java
@@ -4,3 +4,4 @@ public class pyramidPowerTheStarMountain {
     public static void main(String[] args) {
 
         int NumberOfRows = 5;
+//=================================================================

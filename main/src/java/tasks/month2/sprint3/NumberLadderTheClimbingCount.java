f17caecea2d753d4ae4acb22700aa617b1dcb8f5
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-05 11:28:57 +0400

    Add comment separator and print statement

--- main/src/java/tasks/month2/sprint3/NumberLadderTheClimbingCount.java
+++ main/src/java/tasks/month2/sprint3/NumberLadderTheClimbingCount.java
@@ -4,3 +4,5 @@ public class NumberLadderTheClimbingCount {
     public static void main(String[] args) {
 
         int numOfrows = 5;
+        //==============================================================
+        System.out.println("Number Ladder: The Climbing Count");

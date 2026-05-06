a9bd4edee7d46e5bb29b73faf84cb696018b2431
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-06 09:22:03 +0400

    Declare core maze storage variables in main

--- CodeLine/main/src/java/tasks/month2/sprint4/data/EscapingMaze.java
+++ CodeLine/main/src/java/tasks/month2/sprint4/data/EscapingMaze.java
@@ -9,6 +9,11 @@ import java.util.Stack;
 
 public class EscapingMaze {
     public static void main(String[] args) {
+        // ─────────────────────────────────────────────────
+        // STEP 1 — LOAD & SHOW MAZE.TXT IN 2D ARRAY .
+        // ─────────────────────────────────────────────────
+        Path mazePath = null;
+        char array2d[][] = null;
     }
 
     public static void printMaze(char[][] maze, int rows, int cols) {

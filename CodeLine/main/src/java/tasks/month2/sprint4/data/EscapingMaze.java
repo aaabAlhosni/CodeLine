44d43d36eec845f2083580da1badfa46b499eb0e
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-06 09:23:13 +0400

    Add detection logic for '@' and 'E' characters

--- CodeLine/main/src/java/tasks/month2/sprint4/data/EscapingMaze.java
+++ CodeLine/main/src/java/tasks/month2/sprint4/data/EscapingMaze.java
@@ -63,6 +63,14 @@ public class EscapingMaze {
 
         for (int row = 0; row < array2d.length; row++) {
             for (int col = 0; col < array2d[row].length; col++) {
+                if (array2d[row][col] == '@') {
+                    startrow = row;                 // found start row
+                    startColumn = col;                 // found start col
+                }
+                if (array2d[row][col] == 'E') {
+                    exitRow = row;                  // found exit row
+                    exitcolum = col;                  // found exit col
+                }
             }
         }
     }

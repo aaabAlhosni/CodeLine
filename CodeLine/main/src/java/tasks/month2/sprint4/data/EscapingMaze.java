91ad1c4d0b7cab063670a4723d01f061499035b0
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-06 09:21:38 +0400

    Initialize main entry point and helper function stubs

--- CodeLine/main/src/java/tasks/month2/sprint4/data/EscapingMaze.java
+++ CodeLine/main/src/java/tasks/month2/sprint4/data/EscapingMaze.java
@@ -1,4 +1,15 @@
 package tasks.month2.sprint4.data;
 
 public class EscapingMaze {
+    public static void main(String[] args) {
+    }
+
+    public static void printMaze(char[][] maze, int rows, int cols) {
+    }
+
+    public static void printspaces(){
+    }
+
+    public static void delayTime(long seconds){
+    }
 }

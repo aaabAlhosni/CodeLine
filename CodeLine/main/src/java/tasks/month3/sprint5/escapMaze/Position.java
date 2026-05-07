95f5d3775f046cd181b787772c4506f982dd71d2
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-07 12:23:46 +0400

    Close comment block

--- CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/Position.java
+++ CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/Position.java
@@ -6,3 +6,4 @@ package tasks.month3.sprint55.escpeMaze;
 public class Position {
     // ─────────────────────────────────────────
     // STEP 2 — DECLARE START/END POSITION .
+    // ─────────────────────────────────────────

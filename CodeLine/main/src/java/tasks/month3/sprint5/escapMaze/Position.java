267e4fca05a45669dfb9c1c3e30511321b66610c
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-07 12:23:48 +0400

    Declare startColumn static variable

--- CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/Position.java
+++ CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/Position.java
@@ -14,3 +14,4 @@ public class Position {
     //oop object varriables
     private static char[][] array2d = MazeLoader.getArray2d();
     private static int startrow = 0;
+    private static int startColumn = 0;    //start

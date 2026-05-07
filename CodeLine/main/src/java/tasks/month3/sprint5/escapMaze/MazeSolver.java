c64d7c4a73e0b42ccedbd6d5d994c687c59f4f1f
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-07 12:24:24 +0400

    Mark start as visited

--- CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/MazeSolver.java
+++ CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/MazeSolver.java
@@ -66,3 +66,4 @@ public class MazeSolver {
         // -----------------------------------------------
         // STEP 4 — PUSH START AND UPDATE CURRENT POSITION
         stack.push(new int[]{startrow, startColumn});  // push '@' position onto stack
+        visited[startrow][startColumn] = true;          // mark start as visited

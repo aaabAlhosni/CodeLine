e3e633d26addb9a02e8116b7cbdc04a4558bec63
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-07 12:24:26 +0400

    Close startMaze method

--- CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/MazeSolver.java
+++ CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/MazeSolver.java
@@ -72,3 +72,5 @@ public class MazeSolver {
         System.out.println("start solving\n");
         System.out.printf("maze - (%d,%d) current position\n",
                 startColumn, startrow);                 // print as (x,y) == (col,row)
+
+    }

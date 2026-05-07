2f729bb63caee7b127c482f25de01fa8fc1f8325
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-07 12:24:56 +0400

    Add row loop

--- CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/MazeRender.java
+++ CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/MazeRender.java
@@ -62,3 +62,4 @@ public class MazeRender {
     //  EXTERNAL FUNCTIONS — time delay - making spaces - printing maze
     // ────────────────────────────────────────────────────────────────
     public static void printMaze(char[][] maze, int rows, int cols) {
+        for (int row = 0; row < rows; row++) {

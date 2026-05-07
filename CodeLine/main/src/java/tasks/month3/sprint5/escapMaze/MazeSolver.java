288bcf0e1fc83b47858f8c62d0d1f177cd369c75
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-07 12:24:34 +0400

    Check for dead end

--- CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/MazeSolver.java
+++ CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/MazeSolver.java
@@ -120,3 +120,4 @@ public class MazeSolver {
                     System.out.printf(" (%d,%d) current position\n",
                             nextColumn, nextRow);                     // print as (x,y)  (col,row)
                 }
+            }

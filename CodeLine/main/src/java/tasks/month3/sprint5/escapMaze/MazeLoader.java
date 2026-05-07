3f063d0697c6acec8d4177a886595d23bbbe2b07
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-07 12:24:00 +0400

    Begin try block for resource

--- CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/MazeLoader.java
+++ CodeLine/main/src/java/tasks/month3/sprint5/escapMaze/MazeLoader.java
@@ -13,3 +13,4 @@ public class MazeLoader {
     //------------------------------------------------------------------------------------
     // method to Load maze.txt
     public static char[][] loadMaze(){
+        try {

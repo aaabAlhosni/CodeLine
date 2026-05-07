06efabcf5ebd6eef55ba52990aef05bbdc672111
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-05-07 11:51:08 +0400

    Add argument length validation

--- CodeLine/main/src/java/tasks/month3/sprint5/Pick3Easiest.java
+++ CodeLine/main/src/java/tasks/month3/sprint5/Pick3Easiest.java
@@ -8,3 +8,7 @@ import java.util.Arrays;
 
 public class Pick3Easiest {
     public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Usage: java InternQueueSorter <comma-separated-numbers>");
+        }
